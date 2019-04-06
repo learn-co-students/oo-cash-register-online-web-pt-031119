0a5f135cc52187cdbe94290869800bbecc308c73
Mark Kanko <mkanko66@gmail.com>
2019-04-05 18:05:23 -0700

    completed oo-cash-register lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,42 +1,58 @@
+require 'pry'
+
+
 class CashRegister
 
-  attr_accessor :total, :last_trans
+  attr_accessor :total, :discount, :last_trans 
 
-  def initialize(discount = 0)
-    @total = 0
+  def initialize(discount = 0) 
+    @total = 0 
+    #binding.pry 
     @discount = discount
-    @items = []
     @last_trans = 0
-  end
+    @items = [] 
 
-  def discount
-    @discount
-  end
+  end 
 
-  def add_item(title, price)
-    # @total += price
+  # def discount
+  #   @discount 
+  # end 
+
+  # def total
+  #   @total
+  # end 
+
+  def add_item (item, price, quantity = 1)    
     self.total += price * quantity
-    self.last_trans = self.total
-    quantity.times do
-      @items << title
-    end
+    self.last_trans = self.total 
+    while quantity > 0
+      @items << item
+      quantity -= 1
+    end  
+    #binding.pry 
   end
 
+  # quantity.times do
+  #   @items << item
+
+  
+  
   def apply_discount
-    self.total -= self.discount*10
-    "After the discount, the total comes to $#{self.total}."
+    self.total = self.total - @discount * 10
+    if @discount > 0    
+    #binding.pry  
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply." 
+    end 
   end
+  
+  def items 
+    @items 
+  end 
 
-  def items
-    @item
-  end
-
-  def void_last_transaction(price, quantity)
-    #self.total -= self.trans
-    self.last_trans -= (price * quantity)
+  def void_last_transaction
     self.total -= self.last_trans
-  end
-
-
+  end 
 
 end 
