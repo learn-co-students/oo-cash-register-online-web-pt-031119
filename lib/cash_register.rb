8f62c2910bf5b66d4b41f6cfa81332e040dd69f9
Patrick A <akolo@hotmail.com>
2019-04-11 19:58:12 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,9 @@
-
+class CashRegister
+  attr_accessor :total, :add_item, :apply_discount, :items  
+  
+  def initialize (total = 0)
+  @total = total
+  end 
+  def cash_register_with_discount
+    apply_discount = @total *0.20
+end 
